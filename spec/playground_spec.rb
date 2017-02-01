206de5bb6cb6f53f5ec3164145c511109b375e29
Cess <cessmbuguar@gmail.com>
2017-02-01 23:17:51 +0300

    add functionality test

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -2,7 +2,15 @@ require_relative '../lib/playground'
 
 describe Playground do
   context 'when there are no children' do
-  it 'is quite boring'
+
+  it 'is quite boring' do
+    playground = Playground.new
+    mood = playground.mood
+  expect(mood).to  eq('boring')
+
+  end
+
   it 'is empty '
   end
 end
+
