bb53a0eece8c943ea5a1bc1cb3f5e2dbb1253a51
Cess <cessmbuguar@gmail.com>
2017-02-03 12:44:26 +0300

    refactoring playground test code

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -3,16 +3,15 @@ require_relative '../lib/playground'
 
 describe Playground do
   context 'when there are no children' do
+    let(:playground){  Playground.new(0)}
 
     it 'is quite boring place' do
-      playground = Playground.new(0)
       mood = playground.mood
       expect(mood).to  eq('boring')
 
     end
 
     it 'is empty' do 
-      playground = Playground.new(0)
       expect(playground).to be_empty
     end
   end
