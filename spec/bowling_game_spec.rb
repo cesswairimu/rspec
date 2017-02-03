f8ce37cd54edf976c4266c10d9beefc1aa6f50a2
Cess <cessmbuguar@gmail.com>
2017-02-03 12:54:39 +0300

    test for a zero score

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,3 +1,11 @@
 require_relative '../lib/bowling_game'
 describe BowlingGame do
+  it 'scores a gutter game' do
+    game = BowlingGame.new
+    game.pin([0] * 20)
+    expect(game.score).to eq(0)
+  end
+
+  it 'scores a game of 1s' do
+  end
 end
