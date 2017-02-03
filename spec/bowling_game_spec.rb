934ad659ee060e51efca27e480bc890e8cc11af5
Cess <cessmbuguar@gmail.com>
2017-02-03 13:04:02 +0300

    situation for one scores

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,11 +1,13 @@
 require_relative '../lib/bowling_game'
 describe BowlingGame do
+  let(:game){ BowlingGame.new }
   it 'scores a gutter game' do
-    game = BowlingGame.new
     game.pin([0] * 20)
     expect(game.score).to eq(0)
   end
 
   it 'scores a game of 1s' do
+    game.pin([1] * 20)
+    expect(game.score).to eq(20)
   end
 end
