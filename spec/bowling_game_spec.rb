f72a7a20bdbe31eeb40ed89280e393878bebeac1
Cess <cessmbuguar@gmail.com>
2017-02-03 14:36:47 +0300

    pending the spare condition

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/bowling_game'
 describe BowlingGame do
-  let(:game){ BowlingGame.new }
+  let(:game) { BowlingGame.new }
+
   it 'scores a gutter game' do
     game.pin([0] * 20)
     expect(game.score).to eq(0)
@@ -8,6 +9,12 @@ describe BowlingGame do
 
   it 'scores a game of 1s' do
     game.pin([1] * 20)
+    expect(game.score).to eq(20)
+  end
+
+  it 'scores a game with spares' do
+    pending
+    game.pin([4,6,5] + [0]* 17)
     expect(game.score).to eq(20)
   end
 end
