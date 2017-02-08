0b7e3d5363779d1e07862b8f1c284f1f6ab759fc
Cess <cessmbuguar@gmail.com>
2017-02-08 16:40:53 +0300

    remove achievement's spelling error :smiley:

--- i-rock/config/routes.rb
+++ i-rock/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :achievent, only: [:new, :create]
+  resources :achievement, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
