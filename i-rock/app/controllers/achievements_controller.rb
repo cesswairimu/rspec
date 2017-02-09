51774d668b86e87316f864fab6ae669b321e9172
Cess <cessmbuguar@gmail.com>
2017-02-09 16:09:56 +0300

    create action

--- i-rock/app/controllers/achievements_controller.rb
+++ i-rock/app/controllers/achievements_controller.rb
@@ -2,4 +2,16 @@ class AchievementsController < ApplicationController
   def new
     @achievement = Achievement.new
   end
+
+  def create
+    @achievement = Achievement.new(ach_params)
+    if @achievement.save
+      redirect_to root_url, notice: "Achievement has been created"
+    end
+  end
+
+  private
+  def ach_params
+    params.require(:achievement).permit( :title, :description, :featured, :cover_image, :privacy )
+  end
 end
