64bdd0c549a47b453b782f51255d20164ced8082
Cess <cessmbuguar@gmail.com>
2017-02-12 08:40:32 +0300

    add route to show action

--- i-rock/config/routes.rb
+++ i-rock/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'achievements/new'
 
   root 'welcome#index'
-  resources :achievements, only: [:new, :create]
+  resources :achievements, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
