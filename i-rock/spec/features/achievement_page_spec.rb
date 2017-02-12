7521994958519d58980fadd164f5b03f7cdde8e1
Cess <cessmbuguar@gmail.com>
2017-02-12 08:42:30 +0300

    scenario for description

--- i-rock/spec/features/achievement_page_spec.rb
+++ i-rock/spec/features/achievement_page_spec.rb
@@ -4,6 +4,13 @@ feature 'achievement page' do
  scenario 'achievement public page' do
    achievement = Achievement.create(title: 'Just do it')
    visit("/achievements/#{achievement.id}")
-   expect(page).to have_content('Just did it')
+   expect(page).to have_content('Just do it')
  end
+
+ scenario 'render markdown description' do
+ achievement = Achievement.create(title:'title', description: 'That *was* hard')
+   visit("/achievements/#{achievement.id}")
+   expect(page).to have_content('<i>was</i>')
+end
+ 
 end
