7025cdc8c6505ffe4a4dbfb29c8bf3a3006e648b
Cess <cessmbuguar@gmail.com>
2017-02-13 10:03:02 +0300

    proper rendering description tests and template

--- i-rock/spec/features/achievement_page_spec.rb
+++ i-rock/spec/features/achievement_page_spec.rb
@@ -11,7 +11,7 @@ feature 'achievement page' do
  scenario 'render markdown description' do
    achievement =FactoryGirl.create(:achievement, description: 'That *was* hard') 
    visit("/achievements/#{achievement.id}")
-   expect(page).to have_content('<i>was</i>')
+   expect(page).to have_css('em', text: 'was')
 end
  
 end
