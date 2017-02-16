a23518979c6951b57ab9b5df4878928af667459e
Cess <cessmbuguar@gmail.com>
2017-02-16 22:01:28 +0300

    test for new action rendering template

--- i-rock/spec/controllers/achievements_controller_spec.rb
+++ i-rock/spec/controllers/achievements_controller_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe AchievementsController, type: :controller do
+RSpec.describe AchievementsController do
+  describe 'GET new' do
+    it "render new  template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+  it "assigns new achivement to @achievement" do
 
+
+
+  end
 
 end
