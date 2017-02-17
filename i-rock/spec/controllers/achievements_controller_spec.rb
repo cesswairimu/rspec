9d9499945fd80ff0486172fa40c26c17434eb572
Cess <cessmbuguar@gmail.com>
2017-02-17 23:51:43 +0300

    :smiley: refactoring create controller tests

--- i-rock/spec/controllers/achievements_controller_spec.rb
+++ i-rock/spec/controllers/achievements_controller_spec.rb
@@ -28,25 +28,28 @@ RSpec.describe AchievementsController do
   
   describe "POST create" do
     context "valid data" do
+      let(:valid_data) { FactoryGirl.attributes_for(:public_achievement) }
       it " redirects to achievements#show" do
-        post :create, achievement: FactoryGirl.attributes_for(:public_achievement)
+        post :create, achievement:  valid_data
         expect(response).to redirect_to(achievement_path(assigns[:achievement]))
       end
 
       it "creates a new achievement in database" do
         expect{
-          post :create, achievement: FactoryGirl.attributes_for(:public_achievement)
+          post :create, achievement: valid_data
         }.to change(Achievement, :count).by(1)
       end
     end
     context "invalid data" do
+      let(:invalid_data){  FactoryGirl.attributes_for(:public_achievement, title: ' ')
+}
      it "renders new template" do
-        post :create, achievement: FactoryGirl.attributes_for(:public_achievement, title: ' ')
-        expect(response).to render_template(:new)
+        post :create, achievement:   invalid_data  
+          expect(response).to render_template(:new)
       end
       it "does not create new achievement" do
         expect{
-        post :create, achievement: FactoryGirl.attributes_for(:public_achievement, title: ' ')
+        post :create, achievement: invalid_data
         }.not_to change(Achievement, :count)
       end
 
