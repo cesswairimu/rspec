b40d59f5667abaa81d539e64fa75437f4ba4ec18
Cess <cessmbuguar@gmail.com>
2017-02-25 21:05:58 +0300

    validate achievement titles

--- i-rock/app/models/achievement.rb
+++ i-rock/app/models/achievement.rb
@@ -1,9 +1,18 @@
 class Achievement < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
+  validate :identical_titles
   enum privacy: [:public_access, :private_access, :friends  ]
 
   def description_html
      Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+  end
+
+  private
+  def identical_titles
+    existing_achievement = Achievement.find_by(title: title)
+    if existing_achievement && existing_achievement.user == user
+      errors.add(title, "You cant have it same title for achievement twice")
+    end
   end
 end
--- i-rock/spec/models/achievement_spec.rb
+++ i-rock/spec/models/achievement_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
+  describe 'validations' do
+    it 'requires title' do
+      achievement = Achievement.new( title: " " )
+      achievement.valid?
+      expect(achievement.errors[:title]).not_to be_empty
+    end
+
+    it "requires title to be unique for a user" do
+      user = FactoryGirl.create(:user)
+      first_achievement = FactoryGirl.create(:public_achievement,title: "First", user: user)
+      new_achievement =Achievement.new(title: "First", user: user)
+      expect(new_achievement.valid?).to be_falsy
+    end
+ it "allows different users to have identical titles" do
+    user = FactoryGirl.create(:user)
+    user1 = FactoryGirl.create(:user)
+      first_achievement = FactoryGirl.create(:public_achievement,title: "First", user: user)
+      new_achievement =Achievement.new(title: "First", user: user1)
+      expect(new_achievement.valid?).to be_truthy
+ end
+      
+   
+  end
+
 end
