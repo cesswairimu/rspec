36d951a80c483c9b712af21092d5459c54076a55
Cess <cessmbuguar@gmail.com>
2017-03-08 22:33:48 +0300

    should-matchers to refactor model tests

--- i-rock/app/models/achievement.rb
+++ i-rock/app/models/achievement.rb
@@ -2,18 +2,13 @@ class Achievement < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :user, presence: true
-  validate :identical_titles
+  validates :title, uniqueness: {
+    scope: :user_id,
+      message:"You cant have it same title for achievement twice" }
   enum privacy: [:public_access, :private_access, :friends  ]
 
   def description_html
      Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
   end
 
-  private
-  def identical_titles
-    existing_achievement = Achievement.find_by(title: title)
-    if existing_achievement && existing_achievement.user == user
-      errors.add(title, "You cant have it same title for achievement twice")
-    end
-  end
 end
--- i-rock/spec/models/achievement_spec.rb
+++ i-rock/spec/models/achievement_spec.rb
@@ -2,42 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
   describe 'validations' do
-    it 'requires title' do
-      achievement = Achievement.new( title: " " )
-      achievement.valid?
-      expect(achievement.errors[:title]).not_to be_empty
-    end
-
-    it "requires title to be unique for a user" do
-      user = FactoryGirl.create(:user)
-      first_achievement = FactoryGirl.create(:public_achievement,title: "First", user: user)
-      new_achievement =Achievement.new(title: "First", user: user)
-      expect(new_achievement.valid?).to be_falsy
-    end
-    it "allows different users to have identical titles" do
-      user = FactoryGirl.create(:user)
-      user1 = FactoryGirl.create(:user)
-      first_achievement = FactoryGirl.create(:public_achievement,title: "First", user: user)
-      new_achievement =Achievement.new(title: "First", user: user1)
-      expect(new_achievement.valid?).to be_truthy
-    end
-
-it "requires a user" do
-  new = Achievement.new(title:"cess", user: nil)
-  expect(new.valid?).to be_falsy
-end
-
-it "has belongs_to user association" do
-  #approach 1
-  user = FactoryGirl.create(:user)
-  achievement = FactoryGirl.create(:public_achievement, user: user)
-  expect(achievement.user).to eq(user)
-
-  #approach 2
-  u =Achievement.reflect_on_association(:user)
-  expect(u.macro).to eq(:belongs_to)
-
-end
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title).scoped_to(:user_id).with_message("You cant have it same title for achievement twice") }
+    it { should validate_presence_of(:user) }
+    it {should belong_to(:user)}
   end
 
 end
